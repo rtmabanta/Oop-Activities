6b1095c5b2a544f7843290d838652190b81d6137
rtmabanta <111472801+rtmabanta@users.noreply.github.com>
2023-05-25 15:01:29 +0800

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -1,23 +1,24 @@
 package Vehicle;
 
+//Create subclass Plane, inherit all the accessible methods and variables of the Vehicle class
 public class Plane extends Vehicle{
 	
-	private String wingSpan;
+	private String wingSpan;//declare variables or objects for U2-SpyPlane
 	
 	public Plane(String type, String name, String color, int speed, int price, String wingSpan) {
-		super(type, name, color, speed, price);
-		this.wingSpan = wingSpan;
+		super(type, name, color, speed, price);// Call the constructor of the superclass Vehicle
+		this.wingSpan = wingSpan;//this a setter method for wingSpan
 	}
-	String wingSpan() { //create getter method for tireType
-		return wingSpan; // return the value of tireType
+	String wingSpan() { //create getter method for wingSpan
+		return wingSpan; // return the value of wingSpan
 	}
-	void fly() { // Create a method to simulate driving the Toyota_Vios.
-		System.out.println("I am piloting the U-2 Spy Plane"); //Indicate that user is driving the Toyota Vios
+	void fly() { // Create a method to simulate flying the U2-SpyPlane.
+		System.out.println("I am piloting the U-2 Spy Plane"); //Indicate that pilot is flying the U2-SpyPLane.
 	}
 	
 	@Override //Indicates that this methods override a method in superclass Vehicle
-	void stop() { //create a method to simulate stopping the Toyota_Vios.
-		System.out.println("I stop piloting the U-2 Spy Plane"); //Indicate that user has stopped driving the Toyota Vios
+	void stop() { //create a method to simulate stopping the U2-SpyPlane.
+		System.out.println("I stop piloting the U-2 Spy Plane"); //Indicate that the pilot has stopped flying the U2-SpyPlane
 		
 	}
 }
